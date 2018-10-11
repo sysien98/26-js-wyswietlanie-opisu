a683d8d203a31b784dbd8987df673d7c013b5859
Sylwia Sadowska <sylwiaxsadowska@gmail.com>
2018-10-11 12:30:07 +0200

    wyświetlanie opisu

---
+++ js/main.js
@@ -0,0 +1,6 @@
+let paragraph = document.getElementById('opis');
+let button = document.querySelector('button');
+button.addEventListener('click', function() {
+    document.getElementById('opis').innerHTML = "Akademia 108";
+});
+console.log('button');
